596eeaf2d661b0e6a2751042029f86bc969134d0
sarah <skg@neofilm.us>
2024-08-27 16:01:11 -0400

    Add f-strings, intro & exit functions.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import csv
 
+def intro():
+  print('Welcome to the Spanish and French translator app.\nPlease enter an English word and hit Enter.')
+  print('\nType "done" at any time to exit.')
+
+def exit():
+  print('\nThanks for using the translator app. Have a great day!')
+
+
 translations = {}
 
 with open("translations.csv", "r") as words:
@@ -12,15 +20,18 @@ with open("translations.csv", "r") as words:
 
 done = False
 
-print('Type "done" at any time to exit')
+intro()
 
 while not done:
-  word = input("Type an English word to translate: ")
+  word = input("\nType an English word to translate: ")
   word = word.lower()
 
   if word == "done":
     done = True
+    exit()
   elif word in translations:
-    print(translations[word])
+    print(f'\nSPANISH: {translations[word][0]}')
+    print(f'\nFRENCH: {translations[word][1]}')
   else:
-    print("Translation is not known")
+    print("\nTranslation is not known")
+
